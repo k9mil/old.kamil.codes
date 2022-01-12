2baef97709637a725cbeedbd3be3f0b7faead3e9
Kamil Zak <kamilzak00@gmail.com>
2022-01-12 20:55:05 +0000

    /index/ completed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './web/templates/base/*.html',
     './web/templates/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        'neutral': {
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#e5e5e5',
+        },
+      },
+    },
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 }
