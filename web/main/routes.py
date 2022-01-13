f9a30a5b8b79a83f3cf04dcda64c706cc71b69ae
Kamil Zak <kamilzak00@gmail.com>
2022-01-13 19:40:36 +0000

    further utilized jinja2 to minimize html code

--- web/main/routes.py
+++ web/main/routes.py
@@ -40,10 +40,10 @@ def about():
 @main.route("/projects")
 def projects():
     projects_list = [
-        ["🗺️ statum", "A Twitch streamer-related website. Written in Python + Flask, with MongoDB. Current features include Twitch OAuth integration, personalized dashboard, unique streamer insights & much more."],
-        ["🦅 eagle", "A simple, fast, and fun CLI-based application which functions as a helper to find answers to your programming questions!"],
-        ["👁️ Oculus", "Unmaintained camera scraper for Allegro & OLX to try and catch low priced cameras at user-set prices."],
-        ["⚡ kamil.codes", "My personal web(site) created with Flask, TailwindCSS and nginx."],
+        ["🗺️ statum", "A Twitch streamer-related website. Written in Python + Flask, with MongoDB. Current features include Twitch OAuth integration, personalized dashboard, unique streamer insights & much more.", "https://github.com/k9mil/statum"],
+        ["🦅 eagle", "A simple, fast, and fun CLI-based application which functions as a helper to find answers to your programming questions!", "https://github.com/k9mil/eagle"],
+        ["👁️ Oculus", "Unmaintained camera scraper for Allegro & OLX to try and catch low priced cameras at user-set prices.", "https://github.com/k9mil/oculus-monitor"],
+        ["⚡ kamil.codes", "My personal web(site) created with Flask, TailwindCSS and nginx.", "https://github.com/kamil-codes/kamil.codes"],
     ]
 
     return render_template("projects.html", projects=projects_list)
