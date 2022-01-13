2c141807611b193aef0f789ccfa81318f4431337
Kamil Zak <kamilzak00@gmail.com>
2022-01-13 19:20:08 +0000

    utilized ninja to stop repeating html code

--- web/main/routes.py
+++ web/main/routes.py
@@ -8,7 +8,34 @@ def index():
 
 @main.route("/about")
 def about():
-    return render_template("about.html")
+    film_list = [
+        ['/static//img/film/taxi_driver.jpg', 'Taxi Driver'],
+        ['/static//img/film/good_fellas.jpg', 'Goodfellas'],
+        ['/static//img/film/fight_club.jpg', 'Fight Club'],
+        ['/static//img/film/barry_lyndon.jpg', 'Barry Lyndon'],
+        ['/static//img/film/children_of_men.jpg', 'Children of Men'],
+        ['/static//img/film/la_haine.jpg', 'La Haine'],
+    ]
+
+    album_list = [
+        ["/static//img/albums/mbdtf.jpg", "My Beautiful Dark Twisted Fantasy"],
+        ["/static//img/albums/madvillain.jpg", "Madvillain"],
+        ["/static//img/albums/atrocity_exhibition.jpg", "Atrocity Exhibition"],
+        ["/static//img/albums/slauson_malone.jpg", "Vergangenheitsbewältigung"],
+        ["/static//img/albums/blonde.jpg", "Blonde"],
+        ["/static//img/albums/808s.jpg", "808s & Heartbreak"],
+    ]
+
+    book_list = [
+        ["/static//img/book/1984.jpg", "1984"],
+        ["/static//img/book/howtohidee.jpg", "How to Hide an Empire"],
+        ["/static//img/book/sapiens.jpg", "Sapiens"],
+        ["/static//img/book/steve_jobs.jpg", "Steve Jobs"],
+        ["/static//img/book/technological_slavery.jpg", "Technological Slavery"],
+        ["/static//img/book/bravenewworld.jpg", "Brave New World"],
+    ]
+
+    return render_template("about.html", films=film_list, albums=album_list, books=book_list)
 
 @main.route("/projects")
 def projects():
@@ -18,7 +45,8 @@ def projects():
         ["👁️ Oculus", "Unmaintained camera scraper for Allegro & OLX to try and catch low priced cameras at user-set prices."],
         ["⚡ kamil.codes", "My personal web(site) created with Flask, TailwindCSS and nginx."],
     ]
-    return render_template("projects.html")
+
+    return render_template("projects.html", projects=projects_list)
 
 @main.route("/technologies")
 def technologies():
