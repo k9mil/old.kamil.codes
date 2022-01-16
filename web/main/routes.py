74f24b108ff159227e2109fb1a206ad90bd8e174
Kamil Zak <kamilzak00@gmail.com>
2022-01-16 12:19:28 +0000

    utilized jinja to minimize /about/

--- web/main/routes.py
+++ web/main/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app as app
+import json, os
 
 main = Blueprint('main', __name__)
 
@@ -8,34 +9,11 @@ def index():
 
 @main.route("/about")
 def about():
-    film_list = [
-        ['/static//img/film/taxi_driver.jpg', 'Taxi Driver'],
-        ['/static//img/film/good_fellas.jpg', 'Goodfellas'],
-        ['/static//img/film/fight_club.jpg', 'Fight Club'],
-        ['/static//img/film/barry_lyndon.jpg', 'Barry Lyndon'],
-        ['/static//img/film/children_of_men.jpg', 'Children of Men'],
-        ['/static//img/film/la_haine.jpg', 'La Haine'],
-    ]
+    filename = os.path.join(app.static_folder, 'json/about.json')
+    with open(filename) as about:
+        data = json.load(about)
 
-    album_list = [
-        ["/static//img/albums/mbdtf.jpg", "My Beautiful Dark Twisted Fantasy"],
-        ["/static//img/albums/madvillain.jpg", "Madvillain"],
-        ["/static//img/albums/atrocity_exhibition.jpg", "Atrocity Exhibition"],
-        ["/static//img/albums/slauson_malone.jpg", "Vergangenheitsbewältigung"],
-        ["/static//img/albums/blonde.jpg", "Blonde"],
-        ["/static//img/albums/808s.jpg", "808s & Heartbreak"],
-    ]
-
-    book_list = [
-        ["/static//img/book/1984.jpg", "1984"],
-        ["/static//img/book/howtohidee.jpg", "How to Hide an Empire"],
-        ["/static//img/book/sapiens.jpg", "Sapiens"],
-        ["/static//img/book/steve_jobs.jpg", "Steve Jobs"],
-        ["/static//img/book/technological_slavery.jpg", "Technological Slavery"],
-        ["/static//img/book/bravenewworld.jpg", "Brave New World"],
-    ]
-
-    return render_template("about.html", films=film_list, albums=album_list, books=book_list)
+    return render_template("about.html", data=data)
 
 @main.route("/projects")
 def projects():
